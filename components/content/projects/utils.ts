2831305c0e2f889af9030d64201a355e95f98afb
thelonewolf123 <harishkumar.vellore@gmail.com>
2024-04-09 10:33:29 +0530

    Add new projects and blog post about distributed locks in serverless architecture

--- components/content/projects/utils.ts
+++ components/content/projects/utils.ts
@@ -1,4 +1,17 @@
-export const projects = [
+interface Project {
+    title: string
+    description: string
+    img: string
+    href: string
+    preview?: string
+    github: string
+    alt: string
+    platform: string
+    stack: string
+    delay: number
+}
+
+export const projects: Project[] = [
     {
         title: 'PyFiddle - JsFiddle for Python',
         description: `PyFiddle is a python equivalent for JsFiddle. It has a dependency management system just like jsfiddle so people can work on complex projects and it also has a built-in debugger.`,
@@ -32,6 +45,30 @@ export const projects = [
         alt: 'Projects',
         platform: 'Desktop',
         stack: 'Java, JavaFX, FXML.',
+        delay: 0.1
+    },
+    {
+        title: 'Stardust.app',
+        description:
+            'Proof of concept container deployment platform that runs on top of AWS spot instances. It is designed to be a cost-effective alternative to traditional container deployment platforms.',
+        img: '/stardust.png',
+        href: '/projects/stardust',
+        github: 'https://github.com/thelonewolf123/stardust.app',
+        alt: 'Projects',
+        platform: 'Web',
+        stack: 'Aws, NextJs, Typescript, TailwindCSS, MongoDB, NodeJs, Redis, RabbitMQ, Docker.',
+        delay: 0.1
+    },
+    {
+        title: 'Gym-buddy',
+        description:
+            'Gym-buddy is a android application that helps you to track your workouts.',
+        img: '/gym-buddy.png',
+        href: '/projects/gym-buddy',
+        github: 'https://github.com/thelonewolf123/gym-buddy',
+        alt: 'Projects',
+        platform: 'Web',
+        stack: 'React Native, Typescript, TailwindCSS, RealmDB, PocketBase.',
         delay: 0.1
     }
 ]
